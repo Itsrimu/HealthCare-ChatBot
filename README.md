c854395e51b1f9d0b5c0a7b1ee6245909c2b46f3
Itsrimu <nazminkhairunnessa786@gmail.com>
2025-02-07 08:28:18 +0530

    folder stucture added

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # HealthCare-ChatBot
+''''
+create - python -m venv healthbot 
+Acktivate env - .\healthbot\Scripts\activate
+ Install all dependencies - pip install -r requirements.txt
+
+ ''''
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup  
+
+setup(
+    name="Generative Ai Project",
+    version="0.0.0",
+    author="Khairun Nessa Nazmin",
+    author_email="khairun.n.n.rimu@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
